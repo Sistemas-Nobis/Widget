44eaa1376caac0551c38e9fef3fce50f4b01c5a5
Lazaro Gonzalez <lazaro.gonzalez@nobis.com.ar>
2024-09-19 17:16:07 -0300

    Primer commit

---
+++ afiliados/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import BuscarAfiliadoView, BuscarRetencion
+
+app_name = 'afiliados'
+
+urlpatterns = [
+    path('atencion/<str:dni>/', BuscarAfiliadoView.as_view(), name='buscar_afiliado'),
+    path('retencion/<str:dni>/', BuscarRetencion.as_view(), name='buscar_retencion'),
+]
